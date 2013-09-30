5920faeb037b99e4c27ce011f3d6653dcae835a6
Marcelo <marcellovani@yahoo.co.uk>
2013-10-01 00:56:30 +0100

    Update install.sh
    Updated install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
-git clone --branch 7.x-1.x http://git.drupal.org/project/openenterprise.git
-git checkout -b 7.x-1.0-rc4
-git reset --hard HEAD
-cd openenterprise
-drush make build-openenterprise.make
+#git clone --branch 7.x-1.x http://git.drupal.org/project/openenterprise.git
+#git checkout -b 7.x-1.0-rc4
+#git reset --hard HEAD
+#cd openenterprise
+#drush make build-openenterprise.make
+
+git clone https://github.com/marcelovani/openenterprise.git
+cd scripts
+#drush make --prepare-install openenterprise.make folder_of_your_site
